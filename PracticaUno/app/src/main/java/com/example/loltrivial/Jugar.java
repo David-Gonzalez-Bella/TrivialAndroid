4880bbe9692817db75dcb0bbf5e10db8a4e98649
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-10-18 20:22:00 +0200

    Lista de preguntas
    -El TAD 'Pregunta' ha sido creado, y un archivo que hace de puente ente este y el .java
    -Pantalla basica en la activity de 'Jugar', con los textos puediendo ser ya reemplazados por la informacion de los campos de una pregunta

--- PracticaUno/app/src/main/java/com/example/loltrivial/Jugar.java
+++ PracticaUno/app/src/main/java/com/example/loltrivial/Jugar.java
@@ -3,12 +3,38 @@ package com.example.loltrivial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Jugar extends AppCompatActivity {
+
+    public ArrayList<Pregunta> preguntas;
+    public TextView enunciado;
+    public RadioButton r1;
+    public RadioButton r2;
+    public RadioButton r3;
+    public RadioButton r4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jugar);
+
+        enunciado = findViewById(R.id.Enunciado);
+        r1 = findViewById(R.id.respuesta1);
+        r2 = findViewById(R.id.respuesta2);
+        r3 = findViewById(R.id.respuesta3);
+        r4 = findViewById(R.id.respuesta4);
+
+        preguntas = ListaPreguntas.INSTANCE.getPreguntas();
+
+        enunciado.setText(preguntas.get(0).getPregunta());
+        r1.setText(preguntas.get(0).getOpcion1());
+        r2.setText(preguntas.get(0).getOpcion2());
+        r3.setText(preguntas.get(0).getOpcion3());
+        r4.setText(preguntas.get(0).getOpcion4());
     }
 }
